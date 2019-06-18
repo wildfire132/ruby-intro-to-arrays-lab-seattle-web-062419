66265fedf1a907be803f89097a0e0723bd39f25f
Katana Tran <tkatyk21@gmail.com>
2019-06-18 23:28:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,15 @@ def array_with_two_elements
   array = [1,2]
   return array
 end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
